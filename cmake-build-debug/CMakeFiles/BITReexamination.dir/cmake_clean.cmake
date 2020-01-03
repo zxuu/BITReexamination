3e1511dcdead95573b14e8e9d10d4a358d2161a2
zxu <z18255713582@outlook.com>
2020-01-03 16:39:10 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/BITReexamination.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BITReexamination.exe"
+  "BITReexamination.exe.manifest"
+  "BITReexamination.pdb"
+  "CMakeFiles/BITReexamination.dir/main.c.obj"
+  "libBITReexamination.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BITReexamination.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
