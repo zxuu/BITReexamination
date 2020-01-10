723a0f011370869330ba772088be40b940c7a335
zxu <z18255713582@outlook.com>
2020-01-10 22:14:53 +0800

    数字位置

---
+++ ShuZiWeiZhi.c
@@ -0,0 +1,20 @@
+//
+// Created by zxuu on 2020/1/10.
+//
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int n;
+    char s[50];
+    scanf("%d", &n);
+    getchar();
+    for (int i = 0; i < n; i++) {
+        gets(s);
+        for (int k = 0; k < strlen(s); k++) {
+            if ('0'<=s[k]&&s[k]<='9'){
+                printf("%d ", k+1);
+            }
+        }
+        printf("\n");
+    }
+}
