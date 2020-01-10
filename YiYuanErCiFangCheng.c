140051ab38fefcee4bed16b3b444b56e6bdffa7b
zxu <z18255713582@outlook.com>
2020-01-10 21:26:26 +0800

    一元二次方程

--- YiYuanErCiFangCheng.c
+++ YiYuanErCiFangCheng.c
@@ -8,17 +8,23 @@ int main(){
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         int a,b,c;
+        float temp;
         scanf("%d%d%d", &a, &b, &c);
+        temp=b*b-4*a*c;
         if (a == 0) {
-            printf("x=%.2f\n", (0.0 - c+0.0) / b);
+            printf("x=%.2f\n", -c*1.0/b);
         } else{
-            if (b * b - 4 * a * c < 0) {
+            if (temp < 0) {
                 printf("-1\n");
-            } else if (b * b - 4 * a * c > 0) {
-                printf("x1=%.2f,x2=%.2f\n", ((0 - b+0.0) - sqrt(b * b - 4 * a * c)) / 2.0 * a,
-                       ((0 - b+0.0) + sqrt(b * b - 4 * a * c)) / 2.0 * a);
+            } else if (temp > 0) {
+                float x1,x2;
+                x1=(-1*b*1.0-sqrt(temp))/(2*a);
+                x2=(-1*b*1.0+sqrt(temp))/(2*a);
+                printf("x1=%.2f,x2=%.2f\n",x1,x2);
             } else {
-                printf("x=%.2f\n", (0 - b+0.0) / 2.0 * a);
+                float x;
+                x=-1*b*1.0/(2*a);
+                printf("x=%.2f\n",x);
             }
         }
     }
