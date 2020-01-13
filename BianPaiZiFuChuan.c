411bbcce17ff0f01b64e3db5a0cfd4175176be6b
zxu <z18255713582@outlook.com>
2020-01-13 20:56:08 +0800

    编排字符串2

--- BianPaiZiFuChuan.c
+++ BianPaiZiFuChuan.c
@@ -5,22 +5,26 @@ typedef struct{
     struct StrNode* next;
 }StrNode,*StrList;
 int main(){
-    int n;
+    int n, i, k;
     scanf("%d", &n);
-    StrList head;
-    head = (StrList) malloc(sizeof(StrNode));
-    head->next = NULL;
-    for (int i = 0; i < n; i++) {
-        StrList s = malloc(sizeof(StrNode));
+    StrList head, q;
+    getchar();
+    for (i = 0; i < n; i++) {
+        StrList s = (StrList) malloc(sizeof(StrNode));
         gets(s->s);
-        s->next=head->next;
-        head = s;
-        StrList q=head;
-        int i=0;
-        while (q->next&&i<5) {
-            i++;
-            printf("%d=%s ", i, q->s);
-            q=q->next;
+        k = 0;
+        if (i == 0) {
+            head = s;
+            head->next = NULL;
+        } else{
+            s->next = head;
+            head = s;
+        }
+        q = head;
+        while (q && k < 4) {
+            k++;
+            printf("%d=%s ", k, q->s);
+            q = q->next;
         }
         printf("\n");
     }
--- JiaFaDengShi.c
+++ JiaFaDengShi.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 int main(){
-    printf("hellowoeld");
+    printf("hello word");
 }
