48674e4fbd230d2c21b190a1e96958a1451db5ce
zxu <z18255713582@outlook.com>
2020-01-28 10:23:12 +0800

    DayOfWeek

--- BaoLiQiuJie/MoNi/DayOfWeek.cpp
+++ BaoLiQiuJie/MoNi/DayOfWeek.cpp
@@ -22,35 +22,29 @@ Sunday
 #include <cstdio>
 
 using namespace std;
-int daytag[2][13] = {
-        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-};
 
-bool IsLeapYear(int year){
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-}
-string months[13] ={"Haha","January","February","March","April","May","June","July","August","September","October"
+string months[13] ={"","January","February","March","April","May","June","July","August","September","October"
              ,"November","December"};
-string weeks[8]={"","Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+string weeks[7]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 int main(){
-    int number, weekInt, monthInt;
+    int weekInt, monthInt;
     int year,day;
     string monthStr;
     while (cin >> day >> monthStr >> year) {
-        number = 0;
         for (int i = 0; i < 13; ++i) {
             if (monthStr == months[i]) {
                 monthInt = i;
                 break;
             }
         }
-        int row = IsLeapYear(year);
-        for (int j = 0; j < monthInt; ++j) {
-            number += daytag[row][j];
+        //注意在公式中有个与其他公式不同的地方：把一月和二月看成是上一年的十三月和十四月，例：如果是2004-1-10则换算成：2003-13-10来代入公式计算。
+        //以公元元年为参考，公元元年1月1日为星期一
+        if (1 == monthInt || 2 == monthInt) {
+            monthInt += 12;
+            year--;
         }
-        number += day;
-        weekInt = number % 7 + 1;
+        //基姆拉尔森计算公式:W= (d+1+2*m+3*(m+1)/5+y+y/4-y/100+y/400) mod 7
+        weekInt = (day + 1 + 2 * monthInt + 3 * (monthInt + 1) / 5 + year + year / 4 - year / 100 + year / 400) % 7;
         cout << weeks[weekInt] << endl;
     }
     return 0;
