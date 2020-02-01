e221a97db8541b12acf0b3cce00e3aad9a2586ea
zxuu <z18255713582@outlook.com>
2020-02-01 10:13:20 +0800

    打印极值点下标

--- cmake-build-debug/CMakeFiles/BITReexamination.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/BITReexamination.dir/cmake_clean.cmake
@@ -2,7 +2,7 @@ file(REMOVE_RECURSE
   "BITReexamination.exe"
   "BITReexamination.exe.manifest"
   "BITReexamination.pdb"
-  "CMakeFiles/BITReexamination.dir/SortAndFind/Find/FindMin.cpp.obj"
+  "CMakeFiles/BITReexamination.dir/SortAndFind/Find/JiZhiDian.cpp.obj"
   "libBITReexamination.dll.a"
 )
 
