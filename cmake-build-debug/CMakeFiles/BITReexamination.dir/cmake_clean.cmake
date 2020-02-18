eb2c4dcab14216f459b22a03fa94db92cf1481c8
zxuu <z18255713582@outlook.com>
2020-02-18 11:12:20 +0800

    Math && 最大公约数

--- cmake-build-debug/CMakeFiles/BITReexamination.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/BITReexamination.dir/cmake_clean.cmake
@@ -2,7 +2,7 @@ file(REMOVE_RECURSE
   "BITReexamination.exe"
   "BITReexamination.exe.manifest"
   "BITReexamination.pdb"
-  "CMakeFiles/BITReexamination.dir/Math/JinZhiZhuanHuan/ShuZhiZhuanHuan.cpp.obj"
+  "CMakeFiles/BITReexamination.dir/Math/GCD/gcd.cpp.obj"
   "libBITReexamination.dll.a"
 )
 
