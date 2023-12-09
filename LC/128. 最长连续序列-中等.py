39051f1ccbaed6f77b97817c76630e118e94ff46
zxuu <z18255713582@outlook.com>
2023-12-09 22:29:43 +0800

    49

---
+++ LC/128. 最长连续序列-中等.py
@@ -0,0 +1,46 @@
+'''
+给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+
+请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+
+ 
+
+示例 1：
+
+输入：nums = [100,4,200,1,3,2]
+输出：4
+解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+示例 2：
+
+输入：nums = [0,3,7,2,5,8,4,6,0,1]
+输出：9
+ 
+
+提示：
+
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+'''
+from typing import List
+class Solution(object):
+    def longestConsecutive(self, nums: List[int]) -> int:
+        '''官方：'''
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+
+if __name__ == '__main__':
+    s = Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1])
+    print(s)
--- LC/49. 字母异位词分组-中等.py
+++ LC/49. 字母异位词分组-中等.py
@@ -25,13 +25,15 @@
 0 <= strs[i].length <= 100
 strs[i] 仅包含小写字母
 '''
+import collections
+from typing import List
 class Solution(object):
     def groupAnagrams(self, strs):
-        """
-        字母异位词作为键值
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
+        '''
+        由于互为字母异位词的两个字符串包含的字母相同，
+        因此对两个字符串分别进行排序之后得到的字符串一定是相同的，
+        故可以将排序之后的字符串作为哈希表的键。
+        '''
         unique = dict()
         for _ in strs:
             if len(_)==0:
@@ -46,7 +48,40 @@ class Solution(object):
                     unique[str(sorted(_))].append(_)
         result = unique.values()
         return result
+class Solution1:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        官方-排序
+        由于互为字母异位词的两个字符串包含的字母相同，
+        因此对两个字符串分别进行排序之后得到的字符串一定是相同的，
+        故可以将排序之后的字符串作为哈希表的键。
+        '''
+        mp = collections.defaultdict(list)
+        for st in strs:
+            key = "".join(sorted(st))
+            mp[key].append(st)    # 字符串作为键值 比 字符串数组作为键值速度快
+        
+        return list(mp.values())
+class Solution2:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        官方-计数
+        由于互为字母异位词的两个字符串包含的字母相同，因此两个字符串中的相同字母出现的次数一定是相同的，故可以将每个字母出现的次数使用字符串表示，作为哈希表的键。
+        由于字符串只包含小写字母，因此对于每个字符串，可以使用长度为26的数组记录每个字母出现的次数。
+        需要注意的是，在使用数组作为哈希表的键时，不同语言的支持程度不同，因此不同语言的实现方式也不同。
+        '''
+        mp = collections.defaultdict(list)
+
+        for st in strs:
+            counts = [0] * 26
+            for ch in st:
+                counts[ord(ch) - ord("a")] += 1    # ord()：Return the Unicode code point for a one-character string.
+            # 需要将 list 转换成 tuple 才能进行哈希
+            mp[tuple(counts)].append(st)
+        
+        return list(mp.values())
+
 
 if __name__ == '__main__':
-    s = Solution().groupAnagrams(["ddddddddddg","dgggggggggg"])
+    s = Solution2().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
     print(s)
